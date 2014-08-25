9c2e8e7ba2895484cd4d462aaaa1ba233f81b166
Vardhaman Deshpande <vardhaman.rd@gmail.com>
2014-08-25 18:37:16 +0100

    Project to log custom error from Apps

---
+++ LogAppErrors/Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="dd3da619-1a4e-4e53-a9f3-3cd1c263b1ee" description="SharePoint App Feature" featureId="dd3da619-1a4e-4e53-a9f3-3cd1c263b1ee" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="LogAppErrors Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="33d600f8-57f0-4054-a848-b557cfc64ceb" />
+    <projectItemReference itemId="f2776b0f-4942-4b3f-979d-0b88cc67d340" />
+    <projectItemReference itemId="c2aec45e-74d0-4450-99fd-f75286d8ee14" />
+    <projectItemReference itemId="01db5c62-0f2b-4ef7-800e-c2692cb143e8" />
+    <projectItemReference itemId="9a9d43af-b812-4e91-8a29-1cda89b6e799" />
+    <projectItemReference itemId="224f0eed-328a-44de-8be8-6ee80665b963" />
+  </projectItems>
+</feature>
---
+++ LogAppErrors/Scripts/App.js
@@ -0,0 +1,41 @@
+﻿(function ($) {
+
+    'use strict';
+
+    var clientContext;
+
+    $(document).ready(function () {
+
+        clientContext = SP.ClientContext.get_current();
+
+        var myList = clientContext.get_web().get_lists().getByTitle("MyList");
+
+        var itemCreateInfo = new SP.ListItemCreationInformation();
+
+        var listItem = myList.addItem(itemCreateInfo);
+
+        listItem.set_item("Title", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+        listItem.update();
+
+        clientContext.load(listItem);
+
+        clientContext.executeQueryAsync(onSuccess, onFail);
+
+    });
+
+    function onSuccess() {
+        console.log('Item Created');
+    }
+
+    function onFail(sender, args) {
+        var result = SP.Utilities.Utility.logCustomAppError(clientContext, args.get_message());
+        clientContext.executeQueryAsync(function () {
+            console.log('Logging Success');
+        }, function (sender, args) {
+            console.log('Logging Failed');
+            console.log(args.get_message());
+        });
+    }
+
+})(jQuery);
